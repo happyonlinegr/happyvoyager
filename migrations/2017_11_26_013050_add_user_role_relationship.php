e7081ff30408034ee63835091a28ebe45dfacc3c
Δημητρης Τριτσετακης <j.tritsetakis@gmail.com>
2021-02-15 16:29:06 +0200

    Fix migrations

--- migrations/2017_11_26_013050_add_user_role_relationship.php
+++ migrations/2017_11_26_013050_add_user_role_relationship.php
@@ -14,7 +14,7 @@ class AddUserRoleRelationship extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->bigInteger('role_id')->unsigned()->change();
+            $table->unsignedBigInteger('role_id');
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
